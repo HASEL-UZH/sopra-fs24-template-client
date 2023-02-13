f388c134f94f0588a78ce8f3fa9008c16deb63c0
Roy <roy.rutishauser@gmail.com>
2023-02-13 17:27:10 +0100

    Update getDomain.js

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,7 +7,7 @@ import { isProduction } from 'helpers/isProduction'
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'https://sopra-fs23-server.oa.r.appspot.com/' // TODO: insert your prod url for server (once deployed)
+  const prodUrl = 'https://my-server-url.oa.r.appspot.com/' // TODO: insert your prod url for server (once deployed)
   const devUrl = 'http://localhost:8080'
 
   return isProduction() ? prodUrl : devUrl
